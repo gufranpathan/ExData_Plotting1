20f8a6d5ed41c88b8571d38d394273c0fe5dd905
unknown <gufranpathan@outlook.com>
2014-07-12 12:29:13 +0530

    plot2 code and png added

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 # This script assumes that the dataset is in the same folder as the script
-# Description: Create plot1.png
+# Description: Create plot2.png
 
 #Read the data and convert the date and time vairables into POSIX format
 data<- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?",stringsAsFactors=FALSE)
@@ -11,8 +11,8 @@ data$Date<-as.Date(data$Date,format="%d/%m/%y%y")
 dates<-as.Date(c("2007-02-01","2007-02-02"),format="%y%y-%m-%d")
 datesubs<- data[data$Date%in%dates,]
 
-#Make plot1.png
-plot(datesubs$Time,datesubs$Global_active_power,type="l")
+#Make plot2.png
+plot(datesubs$Time,datesubs$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
 par(bg=NA) 
 dev.copy(png,file="plot2.png",width=480,height=480)
 dev.off()
